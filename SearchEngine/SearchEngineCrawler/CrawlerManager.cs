1f8d18335e7d7c1b07f7d8725828c3a6082472b0
kidagine <davidkalatzis1999@hotmail.com>
2021-02-13 12:14:50 +0100

    Initial commit.

---
+++ SearchEngine/SearchEngineCrawler/CrawlerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchEngineCrawler
+{
+	class CrawlerManager
+	{
+		public List<Crawler> CreateCrawlers(string searchWord, Queue<Uri> initialQueue, int amount)
+		{
+			List<Crawler> crawlers = new List<Crawler> { new Crawler(searchWord, initialQueue, 5) };
+			for (int i = 1; i < amount; i++)
+			{
+				crawlers.Add(new Crawler());
+			}
+			return crawlers;
+		}
+
+		public void StopCrawlers(List<Crawler> crawlers)
+		{
+			foreach (Crawler webCrawler in crawlers)
+			{
+				//webCrawler.Stop();
+			}
+		}
+	}
+}
